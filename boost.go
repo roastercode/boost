0f13f300f3842c97f06a5c1c3189d433409acd51
4ur3l13n <aurelien@hackers.camp>
2015-08-30 12:35:49 +0200

    + type Booster interface

--- boost.go
+++ boost.go
@@ -4,7 +4,7 @@ Booster made to works with GoXp
 We do not inject, we boost.
 
 inject -> boost
-Injector -> booster
+Injector -> Booster
 ...
 
 */
@@ -15,3 +15,14 @@ import (
 	"fmt"
 	"reflect"
 )
+// reflection in Go http://blog.golang.org/laws-of-reflection
+
+type Booster interface {
+	Applicator
+	Invoker
+	TypeMapper
+	SetParent(Booster)
+	// SetParent sets the parent of the booster, If the booster cannot find a
+	// dependancy in its Type map it will check its parent before returning an
+	// error.
+}
