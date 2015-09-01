90a19e82cb96015b308810cb8a1d1efdca5ce424
4ur3l13n <aurelien@hackers.camp>
2015-09-01 08:53:22 +0200

    +Applicator +Invoker

--- boost.go
+++ boost.go
@@ -26,3 +26,21 @@ type Booster interface {
 	// dependancy in its Type map it will check its parent before returning an
 	// error.
 }
+
+type Application interface {
+	// Applicator represents an interface for mapping dependencies to a struct.
+	Apply(interface{}) error
+	// Maps dependencies in the Type map to each field in the struct
+	// that is tagged with 'boost', Returns an error if the booster
+	// fails.
+}
+
+type Invoker interface {
+	// Invoker represents an interface for calling functions via reflection.
+	Invoker(interface{}) ([]reflect Value, error)
+	// Invoke attemps to call the interface() provided as a function,
+	// providing dependencies for function arguments based on Type.
+	// Returns a slice of reflect.Value representing the returned value of the
+	// function
+	// Returns an error if the injection fails.
+}
